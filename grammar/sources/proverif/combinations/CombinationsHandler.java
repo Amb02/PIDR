5f931f2e622275c4eb3c182dfcdb326430b7bdea
Ambroise BAUDOT <ambroise.baudot@telecomnancy.eu>
2019-03-22 13:22:08 +0100

    clean CombinationsHandler

--- grammar/sources/proverif/combinations/CombinationsHandler.java
+++ grammar/sources/proverif/combinations/CombinationsHandler.java
@@ -7,7 +7,6 @@ package proverif.combinations;
 import java.util.ArrayList;
 import java.lang.*;
 
-import proverif.combinations.*;
 
 public class CombinationsHandler{
 
@@ -22,37 +21,25 @@ public class CombinationsHandler{
 	}
 
 	private void ParseCombinations(){
-		/**
-		*	indexOfCombinations est une liste où chaque case correspond à une taille de tuple présent dans le fichier
-		**	----Ex:	si fichier = "(a,b) (a,b) (a,b,c,d) (a,b), alors indexOfCombinations=[null,null],
-		**			et indexOfCombinations[0] renseigne les tuples de taille 2 et indexOfCombinations[1] renseigne le tuple de taille 4
-		***
-		*	i=indexOfCombinations[k] est la i-ème combinaison possible pour un tuple dont la taille correspond au k-ième indice de indexOfCombinations
-		**	----Ex:	si indexOfCombinations[1]=1, alors les tuples de taille 2 doivent être parenthésés ((a,b),(c,d))
-		**			A noter que par exemple indexOfCombinations[0] ne peut valoir que 0 car un seul parenthésage possible pour (a,b)
-		***
-		*	parcours : '?'
-		*/
-		ArrayList<Integer> 	sizes				= combinations.getSizes();
-		ArrayList<Integer> 	indexOfCombinations	= new ArrayList(sizes.size());
+
+		ArrayList<Integer> 			sizes					= combinations.getSizes();
+		ArrayList<Integer>			indexOfCombinations		= new ArrayList(sizes.size());
+		int 						numberOfFiles			= combinations.getnumberOfFiles();
+		
 		for (int i : sizes){indexOfCombinations.add(0);} //initialisation
-		int 				numberOfFiles		= combinations.getnumberOfFiles();
+		
 		for (int fileIndex = 0 ; fileIndex < numberOfFiles ; fileIndex++){
+			StringBuilder listOfNewTuples = new StringBuilder("\nFile n°"+fileIndex+" :\n< ");
 			
-			String strTemp="\nFile n°"+fileIndex+" :\n< ";
 			for (Tuple tuple : tuples){
 				int indexOfSize = sizes.indexOf(tuple.size());
-				/**
-				* Le tuple 'tuple' doit être remplacé par liste_des_combinaisons.get(indexOfCombinations.get(indexOfSize))
-				* liste_des_combinaisons = tuple.getcombinations()
-				*/
-				//replace(tuple#.toString#,tuple.getcombinations().get(indexOfCombinations.get(indexOfSize)));
-				strTemp=strTemp+"//"+tuple.getCombinations().get(indexOfCombinations.get(indexOfSize))+"\\\\ , ";
-			}
-			strTemp=strTemp+" >";
-			System.out.println(strTemp);
+				listOfNewTuples.append("//"+tuple.getCombinations().get(indexOfCombinations.get(indexOfSize))+"\\\\ , ");}
 
 			if(fileIndex != numberOfFiles-1) updateIndexOfCombinations(indexOfCombinations,sizes,numberOfFiles);
+
+			listOfNewTuples.append(" >");
+			System.out.println(listOfNewTuples);
+
 		}
 
 	}
@@ -62,13 +49,13 @@ public class CombinationsHandler{
 		int index=-1;
 		int i=0;
 		
-		while ( (index==-1) && (i<numberOfFiles)) {
+		while ( (index==-1) && (i<sizes.size())) {
 			indexOfCombinations.set(i,indexOfCombinations.get(i)+1);
 			if (indexOfCombinations.get(i)==combinations.numberOfCombinations.get(sizes.get(i))){ 
 				indexOfCombinations.set(i,0);
 				i++; //recommence à 0 et incrémente la valeur un rang au-dessus
 			} else {
-				index=i;//the value of infex is useless excepted for the break-condition of the while-loop
+				index=i;//the value of infex is useless excepted for the break-condition of the while-loop (could be binary : -1 or other value)
 			}
 		}
 
