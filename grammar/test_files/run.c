0918872c8ff2128a21692c36f20d464e3626026a
Ambroise BAUDOT <ambroise.baudot@telecomnancy.eu>
2019-03-31 14:46:53 +0200

    found if there is a trace for a file

--- grammar/test_files/run.c
+++ grammar/test_files/run.c
@@ -13,7 +13,14 @@ void usageError(){
 	exit(3);
 }
 
-int runFile(char* file){
+int sizeOfFile(FILE* file){
+	fseek(file, 0, SEEK_END); 
+	int size = ftell(file);
+	fseek(file, 0, SEEK_SET); 
+	return size;
+}
+
+void executeProverif(char* file){
 	char *command[] = {
 		"../../proverif2.00/proverif",
 		file,
@@ -21,10 +28,45 @@ int runFile(char* file){
 	};
 
 	int fileOutput = open(".temp_file", O_CREAT|O_WRONLY, 0002);
-	dup2(fileOutput,1);
 
-	execvp(command[0], command);
-	return 0;
+	pid_t pid = fork();
+
+	if (pid == -1){
+		fprintf(stderr, "Fork-exec error\n" );
+		exit(1);
+	} 
+	else if (pid > 0){
+		int status;
+		waitpid(pid, &status, 0);
+	} else {
+		dup2(fileOutput,1);
+		execvp(command[0], command);
+		fprintf(stderr, "Just after exec\n" );
+		exit(2);
+	}
+}
+
+int isSecure(char* file){
+	FILE* resultsFile = fopen(".temp_file","r");
+	if (resultsFile==NULL){printf("error openning the results\n");}
+
+	int size = sizeOfFile(resultsFile);
+	char * inFile= malloc(size*sizeof(char)); // size/8 ?
+	fread(inFile, 1, size, resultsFile);
+	fclose(resultsFile);
+
+	int secure = (strstr(inFile,"A trace has been found")!=NULL);
+	free(inFile);
+
+	return secure;
+}
+
+
+void runFile(char* file){
+	executeProverif(file);
+	fprintf(stderr, "Exec done\n" );
+
+	if (isSecure(file)){printf("trace found\n");} else {printf("no trace\n");}
 }
 
 int main(int argc, char *argv[]){
@@ -33,7 +75,7 @@ int main(int argc, char *argv[]){
 	if (argc!=2){usageError();}
 	else {filename = argv[1];}
 
-	pid_t parent = getpid();
+	/*pid_t parent = getpid();
 	pid_t pid = fork();
 
 	if (pid == -1){
@@ -49,6 +91,9 @@ int main(int argc, char *argv[]){
 		runFile(filename);
 		fprintf(stderr, "after exec\n" );
 		exit(2);
-	}
+	}*/
+
+	runFile(filename);
+
 	return 0;
 }
